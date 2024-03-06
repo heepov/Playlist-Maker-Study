7b351442117c14f91788cd7e573392c10a871e34
heepov <heeepov@gmail.com>
2024-03-06 16:51:56 +0300

    Add two screens and small buttons logic

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.search_btn).setOnClickListener(
+            object : View.OnClickListener{override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity,"You clicked on Search Button",Toast.LENGTH_SHORT).show()
+            }}
+        )
+
+        findViewById<Button>(R.id.library_btn).setOnClickListener {
+            Toast.makeText(this, "You clicked on ${(it as Button).text} Button", Toast.LENGTH_SHORT).show()
+        }
+        findViewById<Button>(R.id.settings_btn).setOnClickListener {
+            setContentView(R.layout.activity_settings)
+//            Toast.makeText(this, "You clicked on ${(it as Button).text} Button", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
