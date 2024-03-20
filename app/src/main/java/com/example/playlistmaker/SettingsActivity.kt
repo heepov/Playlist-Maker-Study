aeea59c5338aecb8dbb6d2adfdf244623b5461e8
Nikita Arkhipov <heeepov@gmail.com>
2024-03-20 21:43:27 +0300

    second task from sprint 9 add function on settings button - share app - write to Support - read terms

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,46 +1,71 @@
 package com.example.playlistmaker
 
 import android.content.Context
-import android.content.res.Configuration
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.util.Log
 import android.widget.ImageView
-import android.widget.Switch
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDelegate
 
 class SettingsActivity : AppCompatActivity() {
+    private lateinit var vibrator: Vibrator
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
-        val sharedPreferences = getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
-        val switcher = findViewById<Switch>(R.id.swTheme)
-        val currentNightMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+        vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
 
-        val isDarkMode = sharedPreferences.getBoolean(
-            "IS_DARK_THEME",
-            when (currentNightMode) {
-                Configuration.UI_MODE_NIGHT_YES -> true
-                Configuration.UI_MODE_NIGHT_NO -> false
-                else -> false
+        findViewById<ImageView>(R.id.iwShareApp).setOnClickListener {
+            Intent(Intent.ACTION_SEND).run {
+                setType("text/plain")
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.shareLink))
+                startActivity(this)
             }
-        )
-
-        Toast.makeText(this, "Is Dark Mode: $isDarkMode", Toast.LENGTH_SHORT).show()
-
-        switcher.isChecked = isDarkMode
-
-        switcher.setOnCheckedChangeListener { _, isChecked ->
-            sharedPreferences.edit().putBoolean("IS_DARK_THEME", isChecked).apply()
-            if (isChecked) {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-            } else {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-            }
+            vibrate()
         }
 
-        findViewById<ImageView>(R.id.btnBack).setOnClickListener {
+        findViewById<ImageView>(R.id.iwWriteSupport).setOnClickListener {
+            Intent(Intent.ACTION_SENDTO).run {
+                data = Uri.parse("mailto:")
+                putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.supportEmailAddress)))
+                putExtra(
+                    Intent.EXTRA_SUBJECT,
+                    getString(R.string.supportMailSubject)
+                )
+                putExtra(
+                    Intent.EXTRA_TEXT,
+                    getString(R.string.supportMailText)
+                )
+                startActivity(this)
+            }
+            vibrate()
+        }
+        findViewById<ImageView>(R.id.iwTerms).setOnClickListener {
+            Intent(Intent.ACTION_VIEW).run {
+                setData(Uri.parse(getString(R.string.termsLink)))
+                startActivity(this)
+            }
+            vibrate()
+        }
+
+        findViewById<ImageView>(R.id.iwBack).setOnClickListener {
+            vibrate()
             finish()
         }
+    }
+
+    private fun vibrate() {
+        if (vibrator.hasVibrator()) {
+            // set vibration on 50 milliseconds
+            vibrator.vibrate(VibrationEffect.createOneShot(50, VibrationEffect.DEFAULT_AMPLITUDE))
+        }
+    }
+
+    override fun onDestroy() {
+        // destroy vibrator
+        vibrator.cancel()
+        super.onDestroy()
     }
 }
